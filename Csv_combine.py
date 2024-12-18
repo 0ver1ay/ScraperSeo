6a289a3eb1165c03368c2931ac499edeef6a4f72
0ver1ay <107620656+0ver1ay@users.noreply.github.com>
2024-12-18 23:41:25 +0300

    Update Csv_combine.py

--- Csv_combine.py
+++ Csv_combine.py
@@ -4,10 +4,8 @@ import datetime
 import shutil
 
 def combine_csv_files(directory, output_directory):
-    # Получаем все CSV-файлы из директории
     csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
 
-    # Проверяем, найдены ли CSV-файлы
     if not csv_files:
         print("В директории не найдено CSV-файлов.")
         return
@@ -15,23 +13,21 @@ def combine_csv_files(directory, output_directory):
     combined_rows = []
 
     for file in csv_files:
-        print(f"Обработка файла {file}...")  # Отладочное сообщение
+        print(f"Обработка файла {file}...")
 
         with open(os.path.join(directory, file), 'r', encoding='utf-8') as infile:
             reader = csv.reader(infile)
 
             # Пропускаем первые три ряда
             for _ in range(3):
-                next(reader, None)  # None - это значение по умолчанию, если итератор исчерпан
+                next(reader, None)
 
             for row in reader:
                 combined_rows.append(row)
 
-    # Создаем имя файла с текущей датой
     current_date = datetime.datetime.now().strftime('%Y-%m-%d')
     output_file = os.path.join(output_directory, f"combined_{current_date}.csv")
 
-    # Записываем объединенные ряды в результирующий файл
     with open(output_file, 'w', newline='', encoding='utf-8') as outfile:
         writer = csv.writer(outfile)
         writer.writerows(combined_rows)
